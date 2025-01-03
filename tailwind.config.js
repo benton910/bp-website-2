57c0f9785aae613f29bbdf20a494ecd0055d1d51
Benton Parker <bentonparker@Bentons-MacBook-Air.local>
2025-01-03 12:25:36 -0600

    Added send email in lib, updated the tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  },
+  plugins: [],
+};
